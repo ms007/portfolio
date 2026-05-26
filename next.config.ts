b0e5db945f0bf46d3e70fea4cb59c60ada5637f1
Marc Seiferle <ms007@gmx.ch>
2026-05-26 19:37:50 +0200

    add resume

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/resume.pdf",
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }],
+      },
+    ]
+  },
 }
 
 export default nextConfig
